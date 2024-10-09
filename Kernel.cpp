7be2b8bbece9a32f787ce1dfc9aeda87a5aeabc4
MongHwa <dbgusdn_012@daum.net>
2024-10-09 16:18:27 +0900

    커널에서 픽셀 그리기 예시 코드

--- Kernel.cpp
+++ Kernel.cpp
@@ -1,4 +1,14 @@
+//uintX_t 자료형 사용 위해 include
+#include <cstdint>
+
 //C언어 형식으로 함수 정의
-extern "C" void KernelMain() {
+extern "C" void KernelMain(uint64_t frame_buffer_base,
+                           uint64_t frame_buffer_size) {
+    
+    uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
+    for(uint64_t i = 0; i < frame_buffer_size; i++) {
+        frame_buffer[i] = i % 256;
+    }
+
     while (1) __asm__("hlt");
 }
